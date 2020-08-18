d1ee378d4215187b38cdb786c4cb9bcd53298f33
mmgl <muhammedmustafagull@gmail.com>
2020-08-18 10:35:13 +0300

    üye paneli hata alıyorum tekrar app oluşturup deneyeceğim

--- home/models.py
+++ home/models.py
@@ -71,25 +71,25 @@ class ContactFormu(ModelForm):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(blank=True, max_length=15)
-    address = models.CharField(blank=True, max_length=100)
-    city = models.CharField(blank=True, max_length=50)
-    country = models.CharField(blank=True, max_length=20)
-    image = models.ImageField(blank=True, upload_to='images/users/')
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        phone = models.CharField(blank=True, max_length=15)
+        address = models.CharField(blank=True, max_length=100)
+        city = models.CharField(blank=True, max_length=50)
+        country = models.CharField(blank=True, max_length=20)
+        image = models.ImageField(blank=True, upload_to='images/users/')
 
-    def __str__(self):
-        return self.user.username
+        def __str__(self):
+            return self.user.username
 
-    def user_name(self):
-        return self.user.first_name + ' ' + self.user.last_name + ' [ ' + self.user.username + ' ] '
+        def user_name(self):
+            return self.user.first_name + ' ' + self.user.last_name + ' [ ' + self.user.username + ' ] '
 
-    def image_tag(self):
-        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+        def image_tag(self):
+            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
 
-    image_tag.short_description = 'Image'
+        image_tag.short_description = 'Image'
 
 class UserProfileForm(ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['phone','address','city','country','image']
+        class Meta:
+            model = UserProfile
+            fields = ['phone','address','city','country','image']
